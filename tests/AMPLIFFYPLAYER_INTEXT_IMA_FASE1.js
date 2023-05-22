b369edc2f71caf272b8201e6cf791b353cc53315
Naldo <naldo@ampliffy.com>
2023-05-22 02:03:00 -0700

    core

--- tests/AMPLIFFYPLAYER_INTEXT_IMA_FASE1.js
+++ tests/AMPLIFFYPLAYER_INTEXT_IMA_FASE1.js
@@ -13664,7 +13664,8 @@ var dispatch_check_low_power = function dispatch_check_low_power() {
     alert(JSON.stringify(document.querySelector("#zxcvBNMasdf-video-test").src));
     if (typeof video_promise !== "undefined") {
       video_promise.then(function (res) {})["catch"](function (error) {
-        if (error.match(/NotAllowedError/)) {
+        alert(error);
+        if (error.match(/NotAllowedError/ig)) {
           alert(error);
           (0, _log.cLog)("[event:dispatch_check_low_power] low_power_or_error_with_video");
         }
